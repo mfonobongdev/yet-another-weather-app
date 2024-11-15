7fe7bcece9414619aed82e2f38f342aa6e2b63c0
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2024-11-15 18:45:22 +0100

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			fontFamily: {
+				"geist-sans": ["var(--font-geist-sans)"],
+				"geist-mono": ["var(--font-geist-mono)"],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
